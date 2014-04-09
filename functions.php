9533ffb5c94b10eff0e3d1311c53f04d3f0a3436
Sean Davis <sdavis2702@gmail.com>
2014-04-09 18:31:40 -0500

    edit naming convention and remove redundant code

--- functions.php
+++ functions.php
@@ -81,11 +81,14 @@ add_action( 'widgets_init', 'presentation_widgets_init' );
  * Enqueue scripts and styles.
  */
 function presentation_scripts() {
-	wp_register_style( 'fontawesome', get_stylesheet_directory_uri() . '/inc/fonts/font-awesome/css/font-awesome.min.css' );
-	wp_enqueue_style( 'fontawesome' );
-	wp_enqueue_style( 'sdm-style', get_stylesheet_uri() );
-	wp_enqueue_script( 'sdm-navigation', get_template_directory_uri() . '/inc/js/navigation.js', array(), '20120206', true );
-	wp_enqueue_script( 'sdm-skip-link-focus-fix', get_template_directory_uri() . '/inc/js/skip-link-focus-fix.js', array(), '20130115', true );
+	// main stylesheet
+	wp_enqueue_style( 'presentation-style', get_stylesheet_uri() );
+	// font awesome
+	wp_enqueue_style( 'fontawesome', get_stylesheet_directory_uri() . '/inc/fonts/font-awesome/css/font-awesome.min.css' );
+	// navigation toggle
+	wp_enqueue_script( 'presentation-navigation', get_template_directory_uri() . '/inc/js/navigation.js', array(), '20120206', true );
+	
+	wp_enqueue_script( 'presentation-skip-link-focus-fix', get_template_directory_uri() . '/inc/js/skip-link-focus-fix.js', array(), '20130115', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -142,7 +145,7 @@ function presentation_first_post_class( $classes ) {
 	global $wp_query;
 	if ( 0 == $wp_query->current_post )
 		$classes[] = 'top';
-		return $classes;
+	return $classes;
 }
 add_filter( 'post_class', 'presentation_first_post_class' );
 
